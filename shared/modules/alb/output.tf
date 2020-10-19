c48380c1ce41113251299d75d8789eab822f36cd
mikity_w <m_waragai@enfactory.co.jp>
2020-10-19 11:54:10 +0900

    add #10 ALBの実装完了 ドメインを受け取る形でHTTPSのリスナーは登録する

---
+++ shared/modules/alb/output.tf
@@ -0,0 +1,12 @@
+output "alb_id" {
+  description = "alb ids"
+  value       = "${aws_lb.main_alb.id}"
+}
+output "alb_arn" {
+  description = "alb arn"
+  value       = "${aws_lb.main_alb.arn}"
+}
+output "alb_dns_name" {
+  description = "alb dns name"
+  value       = "${aws_lb.main_alb.dns_name}"
+}
